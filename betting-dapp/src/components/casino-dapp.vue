d95447bcd53318a47f7507a6991a6637656e1d3c
Axel Clark <axelclark2@yahoo.com>
2018-03-11 17:04:20 -0400

    Initial setup: components, router, constants

---
+++ betting-dapp/src/components/casino-dapp.vue
@@ -0,0 +1,17 @@
+<template>
+  <hello-metamask/>
+</template>
+
+<script>
+import HelloMetamask from '@/components/hello-metamask'
+export default {
+  name: 'casino-dapp',
+  components: {
+    'hello-metamask': HelloMetamask
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ betting-dapp/src/components/hello-metamask.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>Hello</p>
+</template>
+
+<script>
+export default {
+  name: 'hello-metamask'
+}
+</script>
+
+<style scoped>
+
+</style>
